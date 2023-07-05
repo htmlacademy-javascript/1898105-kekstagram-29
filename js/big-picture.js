65bc006a549778efbbb134e3e28fee14ff89a2cc
Игнат Пасхин <paskhim_92@mail.ru>
2023-07-05 17:53:24 +0300

    Добавляет комментарии

--- js/big-picture.js
+++ js/big-picture.js
@@ -1,11 +1,28 @@
 import { isEscapeKey } from './util.js';
+import { addComment } from './mock.js';
+
 
 const modalBigPicture = document.querySelector('.big-picture');
 const bigPictureImageContainer = document.querySelector('.big-picture__img');
 const bigPictureImage = bigPictureImageContainer.querySelector('img');
 const pictureCaption = document.querySelector('.social__caption');
-const likesCount = document.querySelector('.social__likes');
+const likesCount = document.querySelector('.likes-count');
 const bigPictureCancel = document.querySelector('.big-picture__cancel');
+const pictureCommentsCount = document.querySelector('.comments-count');
+const pictureCommentsList = document.querySelector('.social__comments');
+
+
+const createComments = () => {
+  const commentPicture = document.querySelector('.social__comment');
+  const commentPictureClone = commentPicture.cloneNode(true);
+  const addCommentPicture = addComment();
+
+  commentPictureClone.querySelector('.social__picture').src = addCommentPicture.avatar;
+  commentPictureClone.querySelector('.social__picture').alt = addCommentPicture.name;
+  commentPictureClone.querySelector('.social__text').textContent = addCommentPicture.message;
+  pictureCommentsList.appendChild(commentPictureClone);
+
+};
 
 const onCloseBigPictureClick = () => {
   modalBigPicture.classList.add('hidden');
@@ -20,8 +37,9 @@ const onBigPictureEscKeyDown = (evt) => {
   }
 };
 
+
 const showBigPicture = (picture) => {
-  const { url, likes, description } = picture;
+  const { url, likes, comments, description } = picture;
 
   modalBigPicture.classList.remove('hidden');
   document.body.classList.add('modal-open');
@@ -29,9 +47,13 @@ const showBigPicture = (picture) => {
   bigPictureImage.src = url;
   pictureCaption.textContent = description;
   likesCount.textContent = likes;
+  pictureCommentsCount.textContent = comments.length;
+
+  createComments();
 
   bigPictureCancel.addEventListener('click', onCloseBigPictureClick);
   document.addEventListener('keydown', onBigPictureEscKeyDown);
+
 };
 
 export { showBigPicture };
--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
 import {addPhotos} from './mock.js';
 import {renderPictures} from './pictures.js';
-import './big-picture.js';
 renderPictures(addPhotos());
